019120d5afec2855b3fa34f9463a5fd576ffe17c
Andrew <93899022+Galahad47@users.noreply.github.com>
2025-01-25 12:36:38 +0300

    Добавил программу для отрисовки годографа Найквиста при условии того что разомкнута.

---
+++ t1/task_noqvist.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+#include <qt4>
+#include <qt4engine>
+#include <QApplication>
+#include <QWidget>
+#include <QPainter>
+
+using namespace std;
+float x1,x2,x3,x4,x5,p,w;
+int main(){
+    x1=120; x2=0,02; x3=0,53; x4=2,25;
+    for(i=0; i>1; i++){
+        p = (1i*2*3.14)/i;
+        w(p)=x1/(x2*p**3+x3*p**2+x4*p+1);
+        return(w,p);
+    };
+    void paintEvent(QPaintEvent *event)
+    {
+        QPainter painter;
+        painter.begin(this);
+        painter.drawLine(W,p);
+        painter.end();
+    }
+
+
+}
